165444c4d5dd48f9e3324c52dfc153c9daddf3fb
rsgz <2966261184@qq.com>
2024-09-29 16:58:21 +0800

    更新了预处理模板的写法，能一步到位

--- Rust_Analysis/muban/0-{}预处理.py
+++ Rust_Analysis/muban/0-{}预处理.py
@@ -1,34 +1,32 @@
 # 下面设置 变量 部分
-v1 = "obj"  # 这个建议设置一个
+v1 = "obj"  # 这个建议设置一个模板变量
 tishi = ""  # 提示部分
 
+def quchu_shouhang(s):
+    lines = s.split('\n')
+    new_text = '\n'.join(lines[1:])  # 去除第一行
+    return new_text
 
 s = r"""
-sentence = f'''\
 print!("{:?}",obj);
 print!("{:#?}",obj);
 println!("{:?}",obj);
 println!("{:#?}",obj);
 dbg!(obj);
 
+// 打印频率
 if i%10000==0 {
     println!("{:?}-->{:?}", i,url);
 }
+"""
+huan = '\n'
+o1 = f'{v1} = ""  # {tishi} '  # obj = "vvv"  # 请输入 打印对象叫啥名字
+o2 = "sentence = f'''\\"+"\n"              # sentence = f"""\
+o3 = r"""
 '''
 print(sentence)
 """
-
-s = f'{v1} = ""  # 请输入 打印对象叫啥名字 '+s
-
+s = o1+huan+o2+quchu_shouhang(s)+quchu_shouhang(o3)
 s = s.replace("{","{{").replace("}","}}")  # 兼容{}语法
-
-# 因为后期改变了那个 第一行 的写法 所以不用去除第一行了
-# lines = s.split('\n')
-# new_text = '\n'.join(lines[1:])  # 去除第一行
-
-# 下面设置 变量 部分
-# v1 = "obj"  # 这个建议设置一个
-# new_text = new_text.replace(v1,f"{{{v1}}}")
-# print(new_text)
-
-print(s)
+new_text = s.replace(v1,f"{{{v1}}}")
+print(new_text)
