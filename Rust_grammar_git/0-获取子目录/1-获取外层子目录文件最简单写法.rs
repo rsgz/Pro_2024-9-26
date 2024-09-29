ad6b36fc8f7ade64d600cc54763f49fbbb108262
rsgz <2966261184@qq.com>
2024-09-29 13:49:37 +0800

    最简单的获取一层子目录的写法

---
+++ Rust_grammar_git/0-获取子目录/1-获取外层子目录文件最简单写法.rs
@@ -0,0 +1,22 @@
+use std::fs;
+use std::io;
+
+fn main() -> io::Result<()> {
+    // let dir_path: &str = r"C:\Users\Administrator\Desktop\data\err"; // 注意路径分隔符需要双斜杠
+    let dir_path: &str = r"\\PC-202407311638\share"; // 注意路径分隔符需要双斜杠
+    for entry in fs::read_dir(dir_path)? { // 正确地遍历 dirset
+        println!("{:?}", entry?.path().display());
+    }
+    Ok(())
+}
+
+/*
+"\\\\PC-202407311638\\share\\0"
+"\\\\PC-202407311638\\share\\00"
+"\\\\PC-202407311638\\share\\斌"
+
+
+上面是获取子目录的最简单的写法
+但是自能获取到 最外层的子目录
+? 只能用在  -> io::Result<()>  和 Ok(())  一起出现的地方
+*/
