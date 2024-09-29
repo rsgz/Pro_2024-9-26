a46a683f7e28a8375b349cfb15908b029523f7cb
rsgz <2966261184@qq.com>
2024-09-29 10:23:28 +0800

    这个文本工具可以将所有的文本文件合并然后去重

--- Text_tool/src/main.rs
+++ Text_tool/src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::path::Path;
+use std::io::{self, BufRead, BufReader};
+
+// 调用这个函数 直接可以获取 文本文件里面  所有行数据
+/*
+let file_path = r"C:\Users\Administrator\Desktop\新技术.txt";
+let mut urls = match get_lines(file_path) {
+    Ok(urls) => urls,
+    Err(e) => {
+        eprintln!("打开文件失败: {}", e);
+        // return Err(e);
+        Vec::new()
+    }
+};
+
+dbg!(urls);
+*/
+
+pub fn get_lines<P>(filename: P) -> io::Result<Vec<String>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    let mut lines = Vec::new();
+    for line in reader.lines() {
+        let line = line?;
+        // 可以选择在这里添加对网址格式的验证
+        lines.push(line);
+    }
+
+    Ok(lines)
+}
+
+
+fn main(){
+    let file_path = r"C:\Users\Administrator\Desktop\data\err\rust_url_err_32568.txt";
+    let mut urls = match get_lines(file_path) {
+        Ok(urls) => urls,
+        Err(e) => {
+            eprintln!("打开文件失败: {}", e);
+            // return Err(e);
+            Vec::new()
+        }
+    };
+
+    dbg!(urls);
 }
