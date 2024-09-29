6688bc318f8b45291f05cc3c07f89302a4fec4df
rsgz <2966261184@qq.com>
2024-09-29 17:19:10 +0800

    调整了模板顺序

---
+++ Rust_Analysis/muban/1-编译属性.py
@@ -0,0 +1,8 @@
+r"""
+#![allow(dead_code)]    // 函数未使用的时候 用
+#![allow(unused_variables)]  // 未使用 变量
+#![allow(unused_imports)]  // 导入不使用
+#![allow(path_statements)]  // x;  的时候用
+#[cfg(test)]  未使用的模块 也可以标记一下 说明是测试的时候用
+#[derive(Debug)]  # 该属性可以应用于 结构体、枚举和联合体 等类型
+"""
--- Rust_Analysis/muban/3-打印.py
+++ Rust_Analysis/muban/3-打印.py
@@ -1,14 +1,15 @@
-obj = "vvv"  # 请输入 打印对象叫啥名字
+obj = "vool"  #
 
-sentence = f"""\
+sentence = f'''\
 print!("{{:?}}",{obj});
 print!("{{:#?}}",{obj});
 println!("{{:?}}",{obj});
 println!("{{:#?}}",{obj});
 dbg!({obj});
 
+// 打印频率
 if i%10000==0 {{
     println!("{{:?}}-->{{:?}}", i,url);
 }}
-"""
+'''
 print(sentence)
