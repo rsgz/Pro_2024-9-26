a89dcea8abff79ff232b597fe62310ebec919042
rsgz <2966261184@qq.com>
2024-09-30 10:55:41 +0800

    增加了navigator方式复制到粘贴板里面

--- Rust_Analysis/muban/JS-JQ/1-表格数据.py
+++ Rust_Analysis/muban/JS-JQ/1-表格数据.py
@@ -17,7 +17,8 @@ function initialization(){{
 }}
 
 function main(){{
-    for(var i=0;i<100;i++){{
+    len = $(`{table} {tr}`).length;
+    for(var i=0;i<len;i++){{
         var tr = $(`{table} {tr}`).eq(i)
         var v = tr.find(`{td}`);
         console.log(v.text()); // 使用.text()来获取文本内容
@@ -33,10 +34,26 @@ async function run() {{
 run();
 
 ﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀ v2 数据复制到粘贴板 ﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀
+// 使用的时候 一定要立即切换到 浏览器主界面 navigator 安全性限制 必须是用户行为
 // 延迟函数
 function delay(ms) {{
     return new Promise(resolve => setTimeout(resolve, ms));
 }}
+
+async function copyTextToClipboard_navigator(text) {{
+    if (!navigator.clipboard) {{
+        // Clipboard API 不可用时回退到旧的复制方法
+        fallbackCopyTextToClipboard(text);
+        return;
+    }}
+    try {{
+        await navigator.clipboard.writeText(text);
+        console.log('Copying to clipboard was successful!');
+    }} catch (err) {{
+        console.error('Could not copy text: ', err);
+    }}
+}}
+    
 
 // 复制字符串到粘贴板函数 复制文本
 function copyTextToClipboard(text) {{
@@ -60,23 +77,27 @@ function initialization(){{
     document.getElementsByTagName('head')[0].appendChild(script);
 }}
 
-function main(){{
+async function main(){{
     var data = "";
-    for(var i=0;i<100;i++){{
+    len = $(`{table} {tr}`).length;
+    for(var i=0;i<len;i++){{
         var tr = $(`{table} {tr}`).eq(i)
         var v = tr.find(`{td}`);
-        console.log(v.text()); // 使用.text()来获取文本内容
-        data.push(v+"\n");
+        console.log(i+1,"-->",v.text()); // 使用.text()来获取文本内容
+        data =data + v.text() + "\\n";
     }}
-    copyTextToClipboard(data)
+    // await delay(1000);
+    // copyTextToClipboard(data);  // 这个超过 1500行就会复制失败
+    copyTextToClipboard_navigator(data);  // navigator 不会有这个限制
 }}
 
 async function run() {{
     initialization()
-    await delay(1000);
+    await delay(3000);
     main();
 }}
 
 run();
+﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀ 代码结束 ﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀﹀
 """
 print(s)
