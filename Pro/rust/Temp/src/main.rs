612f922e9e962db37bea56c7bb9abc783a7e553a
rsgz <52986175+rsgz@users.noreply.github.com>
2024-10-09 20:05:49 +0800

    更新了

--- Pro/rust/Temp/src/main.rs
+++ Pro/rust/Temp/src/main.rs
@@ -1,24 +1,16 @@
-fn largest(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
+struct Point<T> {
+    x: T,
+    y: T,
+}
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
     }
-
-    largest
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
 }
-/*
-The largest number is 100
-The largest number is 6000
-*/
