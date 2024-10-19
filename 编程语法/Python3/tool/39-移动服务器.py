51a72a9f027d19e380ca676156a4895cfeec7fee
rsgz <52986175+rsgz@users.noreply.github.com>
2024-10-19 13:36:55 +0800

    增加移动服务器

---
+++ 编程语法/Python3/tool/39-移动服务器.py
@@ -0,0 +1,9 @@
+from find_pic import find_pic_click_center_keep
+
+im1 = r"D:\Backup\Downloads\008-2.png"
+im2 = r"D:\Backup\Downloads\009.png"
+im3 = r"D:\Backup\Downloads\010.png"
+while 1:
+    find_pic_click_center_keep(small=im1, jieshu_tezheng=None, info='1', xiangsidu=0.98)
+    find_pic_click_center_keep(small=im2, jieshu_tezheng=None, info='1', xiangsidu=0.98)
+    find_pic_click_center_keep(small=im3, jieshu_tezheng=None, info='1', xiangsidu=0.98)
